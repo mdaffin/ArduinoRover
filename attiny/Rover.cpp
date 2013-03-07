eba6741e447c61faba5215c946b1b699d2de67fd
Michael Daffin <james1479@gmail.com>
2013-03-07 19:18:42 +0000

    Fixes compile errors in the attiny rover code

--- attiny/Rover.cpp
+++ attiny/Rover.cpp
@@ -6,17 +6,17 @@ void setup()
     pinMode(8, OUTPUT);
     pinMode(9, INPUT);
     pinMode(10, INPUT);
-    digitalwrite(7, LOW);
-    digitalwrite(8, LOW);
+    digitalWrite(7, LOW);
+    digitalWrite(8, LOW);
     Serial.begin(9600);
-    delay(100);
-    digitalwrite(7, digitalread(9));
-    digitalwrite(8, digitalread(10));
+    delay(1000);
+    digitalWrite(7, digitalRead(9));
+    digitalWrite(8, digitalRead(10));
 }
 
 void loop()
 {
-    digitalWrite(7 digitalRead(9));
-    digitalWrite(8 digitalRead(10));
+    digitalWrite(7, digitalRead(9));
+    digitalWrite(8, digitalRead(10));
     delay(10);
 }
