a1b5fca6362dc3ff23aeeacd1823193c578a69e6
Michael Daffin <james1479@gmail.com>
2013-06-16 17:50:01 +0100

    Fixes the wrong pinout

--- Rover/Rover.ino
+++ Rover/Rover.ino
@@ -9,13 +9,13 @@ Rover rover;
 
 void setup()
 {
-    rover.setLeftWheel(2);
-    rover.setRightWheel(3);
-    rover.setLeftBumper(9);
-    rover.setRightBumper(10);
-    rover.setLED1(8);
-    rover.setLED2(7);
-    rover.setSwitch(5);
+    rover.setLeftWheel(9);
+    rover.setRightWheel(10);
+    rover.setLeftBumper(3);
+    rover.setRightBumper(4);
+    rover.setLED1(7);
+    rover.setLED2(8);
+    rover.setSwitch(2);
 }
 
 void loop()
